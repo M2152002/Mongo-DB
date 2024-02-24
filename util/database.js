d4470c1ad7c3f6bd2e843ce08afe60988f3eb9ba
MAHALAKSHMI G <89649311+M2152002@users.noreply.github.com>
2024-02-24 15:08:48 +0530

    Add files via upload

--- util/database.js
+++ util/database.js
@@ -1,40 +1,29 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-//   dialect: 'mysql',
-//   host: 'localhost'
-// });
-
-// module.exports = sequelize;
-
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-let _db; 
+let _db;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://mahalakshmig21052002:AMnWESe2xSfxCV8m@cluster0.j6fjqjw.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
-.then(client => {
-  console.log('Connected!');
-  _db = client.db();
-  callback();
-})
-.catch(err => {
-  console.log(err);
-  throw err;
-});
-}
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://mahalakshmig21052002:OGuPM4hKPucXLxDx@cluster0.j6fjqjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
 const getDb = () => {
-  if(_db) {
+  if (_db) {
     return _db;
   }
   throw 'No database found!';
-}
+};
 
-// module.exports = mongoConnect;
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-
-
-
